9af47c3a446e63b11deeaf628331daea0dace845
Simon Wang <simon.wang@ssc-spc.gc.ca>
2023-06-09 11:51:26 -0400

    Uncomment sample code in Databricks notebook

--- datahub-admin-reports/datahub-powerbi-report.py
+++ datahub-admin-reports/datahub-powerbi-report.py
@@ -71,34 +71,34 @@ spark.sql(
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC
-# MAGIC create or replace view project_user_count as
-# MAGIC select dept, count(9) as user_count from (
-# MAGIC select case lower(substring(domain, 1, charindex('.', domain)-1)) when 'agr' then 'AAFC' when 'asc-csa'  then 'CSA' when 'cnrc-nrc' then 'NRC' when 'dfo-mpo' then 'DFO' when 'ec' then 'ECCC' when 'inspection' then 'CFIA' when 'nrcan' then 'NRCan' when 'nrcan-rncan' then 'NRCan' when 'nrc-cnrc' then 'NRC'when 'otc-cta' then 'CTA' when 'ssc-spc' then 'SSC' else domain end as dept from (
-# MAGIC SELECT RIGHT (user_name,
-# MAGIC LEN(user_name) - CHARINDEX( '@', user_name)) AS Domain
-# MAGIC FROM project_users) as emails where domain not like 'apption%') as departments group by dept
+ %sql
+
+ create or replace view project_user_count as
+ select dept, count(9) as user_count from (
+ select case lower(substring(domain, 1, charindex('.', domain)-1)) when 'agr' then 'AAFC' when 'asc-csa'  then 'CSA' when 'cnrc-nrc' then 'NRC' when 'dfo-mpo' then 'DFO' when 'ec' then 'ECCC' when 'inspection' then 'CFIA' when 'nrcan' then 'NRCan' when 'nrcan-rncan' then 'NRCan' when 'nrc-cnrc' then 'NRC'when 'otc-cta' then 'CTA' when 'ssc-spc' then 'SSC' else domain end as dept from (
+ SELECT RIGHT (user_name,
+ LEN(user_name) - CHARINDEX( '@', user_name)) AS Domain
+ FROM project_users) as emails where domain not like 'apption%') as departments group by dept
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC
-# MAGIC create or replace view project_workspace_count as 
-# MAGIC select dept, count(9) workspace_count from (
-# MAGIC select Project_Acronym_CD, case lower(substring(domain, 1, charindex('.', domain)-1)) when 'agr' then 'AAFC' when 'asc-csa'  then 'CSA' when 'cnrc-nrc' then 'NRC' when 'dfo-mpo' then 'DFO' when 'ec' then 'ECCC' when 'inspection' then 'CFIA' when 'nrcan' then 'NRCan' when 'nrcan-rncan' then 'NRCan' when 'nrc-cnrc' then 'NRC'when 'otc-cta' then 'CTA' when 'ssc-spc' then 'SSC' else domain end as dept
-# MAGIC from
-# MAGIC (select distinct Project_Acronym_CD, RIGHT (user_name,LEN(user_name) - CHARINDEX( '@', user_name)) as domain 
-# MAGIC from project_users u, projects p 
-# MAGIC where p.project_id = u.project_id 
-# MAGIC and user_name not like '%apption.com') as a
-# MAGIC ) as b group by dept
+ %sql
+
+ create or replace view project_workspace_count as 
+ select dept, count(9) workspace_count from (
+ select Project_Acronym_CD, case lower(substring(domain, 1, charindex('.', domain)-1)) when 'agr' then 'AAFC' when 'asc-csa'  then 'CSA' when 'cnrc-nrc' then 'NRC' when 'dfo-mpo' then 'DFO' when 'ec' then 'ECCC' when 'inspection' then 'CFIA' when 'nrcan' then 'NRCan' when 'nrcan-rncan' then 'NRCan' when 'nrc-cnrc' then 'NRC'when 'otc-cta' then 'CTA' when 'ssc-spc' then 'SSC' else domain end as dept
+ from
+ (select distinct Project_Acronym_CD, RIGHT (user_name,LEN(user_name) - CHARINDEX( '@', user_name)) as domain 
+ from project_users u, projects p 
+ where p.project_id = u.project_id 
+ and user_name not like '%apption.com') as a
+ ) as b group by dept
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC
-# MAGIC create or replace view project_workspace_spend as 
-# MAGIC select distinct Project_Acronym_CD, c.Current as spend 
-# MAGIC from project_credits c, projects p 
-# MAGIC where p.project_id = c.projectid
+ %sql
+
+ create or replace view project_workspace_spend as 
+ select distinct Project_Acronym_CD, c.Current as spend 
+ from project_credits c, projects p 
+ where p.project_id = c.projectid
